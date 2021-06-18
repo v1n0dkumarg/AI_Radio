d9249ab089123d06bc42411a549f3a80aaa17511
Vinodkumar G <vinod28012000@gmail.com>
2021-06-18 13:29:46 +0530

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/airadio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.airadio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/vinod/airadio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.vinod.airadio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
